bfbe86640df2e923ab27b52d50a90dd95d8502da
Shan Khan <60656100+shan3274@users.noreply.github.com>
2023-02-26 16:10:55 +0530

    Add files via upload

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Contact from '../Contact/Contact';
+import Footer from '../Footer/Footer';
+import Imageshow from '../Home/ImageShow/Imageshow';
+import Ourservice from '../Home/Ourservice/Ourservice';
+import Text from '../Home/Text/Text';
+import './Header.css'
+import Navbar from './Navbar';
+const Header = () => {
+  return (
+    <div>
+      <Imageshow/>
+      <Text/> 
+      <Ourservice/>
+      <Contact/>
+      <Footer/>
+      <Navbar/>
+       
+    </div>
+  )
+}
+
+export default Header
