d635b34b8d63b75863505322d9d49531a31deb24
Shan Khan <60656100+shan3274@users.noreply.github.com>
2023-03-01 16:23:14 +0530

    Add files via upload

---
+++ src/components/Product/DataProduct/Dataproduct.js
@@ -0,0 +1,30 @@
+import { useFirebase } from "../../../Context/Firebase";
+import React, { useEffect, useState } from 'react'
+import { useParams } from "react-router-dom";
+import Productpage from "../Productpage/Productpage";
+ 
+
+
+ const Dataproduct = () => {
+    const firebase = useFirebase();
+    const[userData,setUserData] = useState([]);
+    const[username,setUsername] = useState([]);
+    
+    const {id} = useParams()
+    useEffect(()=>{
+        firebase.getList(id).then((user)=>{
+          console.log(user.docs[0].data())
+          setUserData(user.docs)
+        })
+    },[]);
+
+   return (
+     <div>
+      {
+          userData.map((users,key)=><li><Productpage {...users.data()}/></li>)
+      }
+     </div>
+   )
+ }
+ 
+ export default Dataproduct
